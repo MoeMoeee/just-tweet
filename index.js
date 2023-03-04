5e427d1b3f9dc95b86266d9d92093eba80323a76
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-04 13:35:54 +1100

    Add display of tweets to 'home' page

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
+// /index - display all the tweets
+
+// tweets
+const tweets = [
+
+{  
+  username: 'moemoe',
+  tweet: 'The weather is good today!'
+},
+{  
+  username: 'moemoe',
+  tweet: 'The weather is good today!'
+},
+{  
+  username: 'moemoe',
+  tweet: 'The weather is good today!'
+},
+{  
+  username: 'moemoe',
+  tweet: 'The weather is good today!'
+}
+
+]
+
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {tweets})
 })
 
 app.get('/tweet', (req, res) => {

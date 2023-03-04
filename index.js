ca700f738292174c44b993baf5d72fd3539769ac
MoeMoeee <dangkhoa.140502@gmail.com>
2023-03-04 13:01:33 +1100

    Add route for tweet page and view

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.set('views', path.join(__dirname, '/views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/tweet', (req, res) => {
+  res.render('Tweet')
+})
+
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
 })
